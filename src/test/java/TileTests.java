1b1ea8ab93388321524361870fe7f89dd214ffa3
Mohammedhz98 <mohammedhz98@hotmail.com>
2021-08-24 11:07:35 +0100

    source code and test files

--- src/test/java/TileTests.java
+++ src/test/java/TileTests.java
@@ -1,12 +1,96 @@
-import com.company.Tile;
+import com.company.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class TileTests {
 
+    private static Tile empty,bomb,flag,number,hidden,questionmark;
+
+    @BeforeAll
+    public static void setUp() {
+        empty = new Tile();
+        bomb = new BombTile();
+        flag = new FlagTile();
+        number = new NumberTile(4);
+        questionmark = new QuestionMarkTile();
+        hidden = new HiddenTile();
+    }
+
     @Test
     void getTileNumber() {
-        Tile empty = new Tile();
-        Assertions.assertEquals(1,empty.getTileCode(),"test 1");
+        int expectedTileCode = 1;
+        int actualTileCode = empty.getTileCode();
+        Assertions.assertEquals(expectedTileCode,actualTileCode,"The tile numbers are not the same.");
+    }
+
+    @Test
+    void getBombTileNumber() {
+        int expectedTileCode = 2;
+        int actualTileCode = bomb.getTileCode();
+        Assertions.assertEquals(expectedTileCode,actualTileCode,"The tile numbers are not the same.");
+    }
+
+    @Test
+    public void getFlagTileNumber() {
+        int expectedTileCode = 3;
+        int actualTileCode = flag.getTileCode();
+        Assertions.assertEquals(expectedTileCode,actualTileCode,"The tile numbers are not the same.");
+    }
+
+    @Test
+    void getNumberTileNumber() {
+        int expectedTileCode = 6;
+        int actualTileCode = number.getTileCode();
+        Assertions.assertEquals(expectedTileCode,actualTileCode,"The tile numbers are not the same.");
+    }
+
+    @Test
+    void getQuestionmarkTileNumber() {
+        int expectedTileCode = 4;
+        int actualTileCode = questionmark.getTileCode();
+        Assertions.assertEquals(expectedTileCode,actualTileCode,"The tile numbers are not the same.");
+    }
+
+    @Test
+    void tileToString() {
+        String expectedToString = "[ ]";
+        String actualToString = empty.toString();
+        Assertions.assertEquals(expectedToString,actualToString,"The string outputs are not the same.");
+    }
+
+    @Test
+    void bombTileToString() {
+        String expectedToString = "[\uD83D\uDCA3]";
+        String actualToString = bomb.toString();
+        Assertions.assertEquals(expectedToString,actualToString,"The string outputs are not the same.");
+    }
+
+    @Test
+    void flagTileToString() {
+        String expectedToString = "[\uD83D\uDEA9]";
+        String actualToString = flag.toString();
+        Assertions.assertEquals(expectedToString,actualToString,"The string outputs are not the same.");
+    }
+
+    @Test
+    void numberTileToString() {
+        String expectedToString = "[" + 4 + "]";
+        String actualToString = number.toString();
+        Assertions.assertEquals(expectedToString,actualToString,"The string outputs are not the same.");
+    }
+
+    @Test
+    void quesionmarkTileToString() {
+        String expectedToString = "[?]";
+        String actualToString = questionmark.toString();
+        Assertions.assertEquals(expectedToString,actualToString,"The string outputs are not the same.");
+    }
+
+    @Test
+    void hiddenTileToString() {
+        String expectedToString = "[\u25A0]";
+        String actualToString = hidden.toString();
+        Assertions.assertEquals(expectedToString,actualToString,"The string outputs are not the same.");
     }
 }
